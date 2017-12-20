ead6a8b74a30ca45c0aef4044518cfcb3c51d377
felipe castillo <felcast999@gmail.com>
2017-12-20 15:44:23 -0400

    firechat

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+       apiKey: "AIzaSyC0DIqr2hng6vbHG5ebnYwfazHKK3JhYyU",
+       authDomain: "firechat-3ce83.firebaseapp.com",
+       databaseURL: "https://firechat-3ce83.firebaseio.com",
+       projectId: "firechat-3ce83",
+       storageBucket: "firechat-3ce83.appspot.com",
+       messagingSenderId: "787238782709"
+ }
+};
---
+++ src/typings.d.ts
@@ -0,0 +1,8 @@
+/* SystemJS module definition */
+declare var module: NodeModule;
+/// <reference path="../node_modules/firebase/firebase.d.ts" />
+
+interface NodeModule {
+
+  id: string;
+}
